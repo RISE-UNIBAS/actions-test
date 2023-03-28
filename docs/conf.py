27c1554a8ef741d616616eede495c38dceaa870d
MHindermann <50413752+MHindermann@users.noreply.github.com>
2023-03-28 14:40:41 +0200

    setup sphinx to pages test

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'test person (rise@unibas.ch)'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.restbuilder', 'sphinx_autodoc_typehints']
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.restbuilder', 'sphinx_autodoc_typehints', 'sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
